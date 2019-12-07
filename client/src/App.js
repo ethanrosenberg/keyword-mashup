7e01456fcc87d489afb32fc8b58091f40cacaca7
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-12-06 22:32:58 -0800

    Added HomeContainer compoment and installed react-router-dom.

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HomeContainer from './containers/HomeContainer'
+
+
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Switch>
+      <Route exact path="/" component={HomeContainer} />
+    </Switch>
+
     </div>
   );
 }
---
+++ client/src/containers/HomeContainer.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+
+
+
+class HomeContainer extends React.Component {
+  constructor () {
+    super()
+     this.state = {
+       code: '',
+       description: '',
+       status: 'ready',
+       isLoading: true
+
+
+     }
+   }
+
+   render() {
+
+     return {
+
+     }
+   }
+
+ }
+
+ export default HomeContainer
